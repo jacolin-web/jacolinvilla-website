a73ee75dce0330e1ee4e2bf75ad8feb333585086
Jacqueline Linang <Jacque3L@verizon.net>
2025-10-03 15:49:01 -0400

    Updated about copy

--- src/app/sections/About.tsx
+++ src/app/sections/About.tsx
@@ -6,16 +6,18 @@ function About() {
         <div className="flex flex-wrap">
           <h2>Hi, I&rsquo;m Jacque. </h2>
           <br/>
-          <p className="text-md text-center md:text-left md:text-2xl">
-            I&apos;m a frontend engineer with over six years of experience from various enterprise companies and 
-            seed-stage startups such as
-            <a className="inline-link"> Intuit, Synop, and J.P. Morgan </a>.
-          
-            I specialize in bridging design and engineering—crafting intuitive user experiences, 
-            robust design systems and scalable product features. 
-
-            Off the clock, I dabble in <a className="inline-link">writing</a>, 
-            join the occasional <a className="inline-link">game jam</a>, 
+          <p className="text-md text-left md:text-left md:text-2xl">
+            I&apos;m a frontend engineer specializing in crafting intuitive user experiences and 
+             robust product features.
+             <br />
+             <br />
+            I&apos;ve worked in enterprise companies and seed-stage startups such as 
+            <a className="inline-link"> Intuit, Synop, and J.P. Morgan</a> where I helped engineering teams 
+            achieve scalable design systems without slowing down product development or increasing technical debt.
+            <br />
+            <br />
+            Off the clock, I  <a className="inline-link">write</a>, 
+            join <a className="inline-link">game jams</a>, build small <a className="inline-link">web apps</a>,
             and like to <a className="inline-link">make things</a> with my hands.
           </p>
           <div className="flex justify-center md:justify-start gap-1 md:gap-4 text-2xl py-4 w-full">
